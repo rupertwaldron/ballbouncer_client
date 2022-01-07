521290eae38c86d241cef4fa3f3e5a027a746ef6
rupertwaldron <rupert.waldron@yahoo.co.uk>
2022-01-07 14:38:06 +0000

    testserver now ok

--- src/test/java/com/ruppyrup/bigfun/TestServer.java
+++ src/test/java/com/ruppyrup/bigfun/TestServer.java
@@ -1,7 +1,6 @@
 package com.ruppyrup.bigfun;
 
-import javafx.application.Platform;
-import javafx.concurrent.Task;
+import java.util.Set;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,8 +10,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static com.ruppyrup.bigfun.clientcommands.EchoCommands.ADD_PLAYER;
 import static com.ruppyrup.bigfun.clientcommands.EchoCommands.BALL_POSITION;
@@ -20,66 +17,69 @@ import static com.ruppyrup.bigfun.clientcommands.EchoCommands.CO_ORD;
 import static com.ruppyrup.bigfun.clientcommands.EchoCommands.HIT_COUNT;
 import static com.ruppyrup.bigfun.clientcommands.EchoCommands.QUIT;
 import static com.ruppyrup.bigfun.clientcommands.EchoCommands.REMOVE_PLAYER;
-import static com.ruppyrup.bigfun.clientcommands.EchoCommands.TEST;
 
 public class TestServer {
 
-    private boolean enableServer = true;
-    private final Map<String, PrintWriter> clients = new HashMap<>();
+  private boolean enableServer = true;
+  private final Map<String, PrintWriter> clients = new HashMap<>();
 
-    public TestServer() {
+  public TestServer() {
+  }
+
+  public EchoClientHandler startServer(int port) {
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
+      System.out.println("Server is running");
+      while (enableServer) {
+        return new EchoClientHandler(serverSocket.accept());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      System.out.println("Server stopped");
+      stop();
+    }
+    return null;
+  }
+
+  public void stop() {
+    enableServer = false;
+  }
+
+  public void sendBallPosition(double newXPosition, double newYPosition) {
+    clients.forEach((id, writer) -> writer.println(
+        BALL_POSITION + ">" + "all" + "%" + newXPosition + ":" + newYPosition));
+  }
+
+  public void sendHitCount(String id, int count) {
+    clients.get(id).println(HIT_COUNT + ">" + count);
+  }
+
+  public class EchoClientHandler {
+
+    private Socket clientSocket;
+    private String clientId;
+
+    public EchoClientHandler(Socket socket) {
+      this.clientSocket = socket;
+      clientId = socket.getInetAddress() + "::" + socket.getPort();
+      System.out.println("Connected to client " + clientId);
     }
 
-    public String startServer(int port) {
-        try (ServerSocket serverSocket = new ServerSocket(port)){
-            System.out.println("Server is running");
-            while (enableServer)
-                new EchoClientHandler(serverSocket.accept()).run();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("Server stopped");
-            stop();
-        }
-        return "Success";
-    }
+    public int run(String commandToSend) {
+      int finalNumberOfClients = 0;
+      try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+          BufferedReader in = new BufferedReader(
+              new InputStreamReader(clientSocket.getInputStream()))) {
 
-    public void stop() {
-        enableServer = false;
-    }
+        addNewPlayerToServerAndExistingPlayers(out);
+        finalNumberOfClients = clients.keySet().size();
+        System.out.println("Running echo handler");
 
-    public void sendBallPosition(double newXPosition, double newYPosition) {
-        clients.forEach((id, writer) -> writer.println(BALL_POSITION + ">" + "all" + "%" + newXPosition + ":" + newYPosition));
-    }
+//                sendBallPosition(150, 150);
 
-    public void sendHitCount(String id, int count) {
-        clients.get(id).println(HIT_COUNT+ ">" + count);
-    }
+        clients.forEach((id, writer) -> writer.println(commandToSend));
 
-    private class EchoClientHandler {
-        private Socket clientSocket;
-        private String clientId;
-
-        public EchoClientHandler(Socket socket) {
-            this.clientSocket = socket;
-            clientId = socket.getInetAddress() + "::" + socket.getPort();
-            System.out.println("Connected to client " + clientId);
-        }
-
-        public String run() {
-            try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-
-                addNewPlayerToServerAndExistingPlayers(out);
-                System.out.println("Running echo handler");
-
-                sendBallPosition(150, 150);
-
-                clients.forEach((id, writer) -> {
-                    writer.println(QUIT + ">" + clientId);
-//                    out.println(QUIT + ">" + id);
-                });
-
+        clients.forEach((id, writer) -> writer.println(QUIT + ">" + clientId));
 
 //                String inputLine;
 //                while ((inputLine = in.readLine()) != null) {
@@ -89,35 +89,40 @@ public class TestServer {
 //                    }
 //                    updateServerAndPlayersWithUpdatedPlayerLocation(inputLine);
 //                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                stop();
-                System.out.println("Client closed:: " + clientId);
-                clients.remove(clientId);
-                clients.forEach((key, value) -> value.println(REMOVE_PLAYER + ">" + clientId));
-                try {
-                    clientSocket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return "Success";
+      } catch (IOException e) {
+        e.printStackTrace();
+      } finally {
+        stop();
+        System.out.println("Client closed:: " + clientId);
+        clients.remove(clientId);
+        clients.forEach((key, value) -> value.println(REMOVE_PLAYER + ">" + clientId));
+        try {
+          clientSocket.close();
+        } catch (IOException e) {
+          e.printStackTrace();
         }
-
-        private void updateServerAndPlayersWithUpdatedPlayerLocation(String inputLine) {
-            String sendMessage = CO_ORD + ">" +  clientId + "%" + inputLine;
-            String[] xyValues = inputLine.split(":");
-            clients.forEach((id, writer) -> writer.println(sendMessage));
-        }
-
-        private void addNewPlayerToServerAndExistingPlayers(PrintWriter out) {
-            clients.forEach((id, writer) -> {
-                writer.println(ADD_PLAYER + ">" + clientId);
-                out.println(ADD_PLAYER + ">" + id);
-            });
-
-            clients.put(clientId, out);
-        }
+      }
+      return finalNumberOfClients;
     }
+
+
+    public Set<String> getClientIds() {
+      return clients.keySet();
+    }
+
+    private void updateServerAndPlayersWithUpdatedPlayerLocation(String inputLine) {
+      String sendMessage = CO_ORD + ">" + clientId + "%" + inputLine;
+      String[] xyValues = inputLine.split(":");
+      clients.forEach((id, writer) -> writer.println(sendMessage));
+    }
+
+    private void addNewPlayerToServerAndExistingPlayers(PrintWriter out) {
+      clients.forEach((id, writer) -> {
+        writer.println(ADD_PLAYER + ">" + clientId);
+        out.println(ADD_PLAYER + ">" + id);
+      });
+
+      clients.put(clientId, out);
+    }
+  }
 }
--- src/test/java/com/ruppyrup/bigfun/client/EchoClientTest.java
+++ src/test/java/com/ruppyrup/bigfun/client/EchoClientTest.java
@@ -1,10 +1,14 @@
 package com.ruppyrup.bigfun.client;
 
 import com.ruppyrup.bigfun.TestServer;
+import com.ruppyrup.bigfun.TestServer.EchoClientHandler;
+import com.ruppyrup.bigfun.clientcommands.BallPosition;
 import com.ruppyrup.bigfun.clientcommands.Command;
 import com.ruppyrup.bigfun.clientcommands.CommandFactory;
 import com.ruppyrup.bigfun.clientcommands.EchoCommands;
+import com.ruppyrup.bigfun.clientcommands.Quit;
 import com.ruppyrup.bigfun.controllers.ClientController;
+import java.util.Set;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,8 +19,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.concurrent.CompletableFuture;
 
+import static com.ruppyrup.bigfun.clientcommands.EchoCommands.BALL_POSITION;
+import static com.ruppyrup.bigfun.clientcommands.EchoCommands.QUIT;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -26,15 +33,11 @@ import static org.mockito.Mockito.when;
 class EchoClientTest {
 
   private static final int PORT = 3333;
-  private CompletableFuture<String> testServerFuture;
+  private CompletableFuture<EchoClientHandler> testServerFuture;
   private EchoClient echoClient;
 
   @Mock
   CommandFactory commandFactory;
-
-  @Mock
-  private ClientController clientController;
-
 
   @BeforeEach
   void setup() {
@@ -45,21 +48,27 @@ class EchoClientTest {
   @AfterEach
   void pulldown() {
     echoClient.stopConnection();
-    String complete = testServerFuture.join();
-    Assertions.assertEquals("Success", complete);
   }
 
   @Test
-  void sendMessage() {
+  void sendBallPositionMessage() {
 
-    Command mockCommand = mock(Command.class);
+    Command mockBallPositionCommand = mock(BallPosition.class);
+    Command mockQuitCommand = mock(Quit.class);
 
-    when(commandFactory.getCommand(any(EchoCommands.class), anyString())).thenReturn(mockCommand);
+    String commandFromServer = "BALL_POSITION" + ">" + "all" + "%" + 150 + ":" + 150;
+
+    when(commandFactory.getCommand(eq(BALL_POSITION), anyString())).thenReturn(mockBallPositionCommand);
+    when(commandFactory.getCommand(eq(QUIT), anyString())).thenReturn(mockQuitCommand);
+
+    CompletableFuture<Integer> handlerFuture = testServerFuture.thenApplyAsync(
+        handler -> handler.run(commandFromServer));
 
     EchoClientResult echoClientResult = echoClient.startConnection();
-    verify(mockCommand, times(2)).execute();
+    verify(mockBallPositionCommand, times(1)).execute();
+    verify(mockQuitCommand, times(1)).execute();
 
+    Assertions.assertEquals(1, handlerFuture.join());
     Assertions.assertEquals(EchoClientResult.SUCCESS, echoClientResult);
-
   }
 }
