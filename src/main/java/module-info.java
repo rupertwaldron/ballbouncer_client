ce947eb7cacb25b5e3de7871952b91a5ae0630c0
rupertwaldron <rupert.waldron@yahoo.co.uk>
2022-01-10 06:26:48 +0000

    formatting

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module com.ruppyrup.bigfun {
-    requires javafx.controls;
-    requires javafx.fxml;
+  requires javafx.controls;
+  requires javafx.fxml;
 
-    requires com.jfoenix;
+  requires com.jfoenix;
 
-    opens com.ruppyrup.bigfun to javafx.fxml;
-    exports com.ruppyrup.bigfun;
+  opens com.ruppyrup.bigfun to javafx.fxml;
+  exports com.ruppyrup.bigfun;
 
-    exports com.ruppyrup.bigfun.controllers;
-    opens com.ruppyrup.bigfun.controllers to javafx.fxml;
+  exports com.ruppyrup.bigfun.controllers;
+  opens com.ruppyrup.bigfun.controllers to javafx.fxml;
 }
